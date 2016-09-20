5991f6d58f4520c82268664b8a551f948e415dc2
Michael Fegreus <michael.fegreus@gmail.com>
2016-09-20 03:16:57 -0400

    Uploading Treasure Hunt exercise.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	bool didPlayerWin = false;
+	bool north;
+	bool east;
+	bool south;
+	bool west;
+	string directionNS;
+	string directionEW;
+	public Transform playerObject;
+	public Transform treasureObject;
+	public Text statusText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (didPlayerWin) {
+			statusText.text = "YOU GOT TREASURE AND YOU WIN!";
+		} else if ((playerObject.position - treasureObject.position).magnitude < 5f) {
+			statusText.text = "Press [SPACE] to get treasure!";
+			if (Input.GetKey (KeyCode.Space)) {
+				didPlayerWin = true;
+			}
+		} else {    //This compass I created is not pretty or elegant, but it's an attempt at a game mechanic. Game would be 10 times as interesting with collision objects to manuever around.
+			north = false;
+			east = false;
+			south = false;
+			west = false;
+			directionNS = "";
+			directionEW = "";
+			if (playerObject.position.x < treasureObject.position.x){
+				east = true;
+			}
+			if(playerObject.position.x > treasureObject.position.x){
+				west = true;
+			}
+			if(playerObject.position.z < treasureObject.position.z){
+				north = true;
+			}
+			if(playerObject.position.z > treasureObject.position.z){
+				south = true;
+			}
+			if (((playerObject.position.z - treasureObject.position.z) < 5f) && ((playerObject.position.z - treasureObject.position.z) > -5f)) {
+				north = false;
+				south = false;
+			}
+			if (((playerObject.position.x - treasureObject.position.x) < 5f) && ((playerObject.position.x - treasureObject.position.x) > -5f)) {
+				east = false;
+				west = false;
+			}
+			if(north){
+				directionNS = "NORTH";
+			}
+			if (south) {
+				directionNS = "SOUTH";
+			}
+			if (east) {
+				directionEW = "EAST";
+			}
+			if (west) {
+				directionEW = "WEST";
+			}
+			statusText.text = "GO " + directionNS.ToString() + directionEW.ToString();
+			Debug.Log (directionNS);
+			Debug.Log (directionEW);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (moveSpeed * -1f, 0f, 0f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (moveSpeed, 0f, 0f);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0f, 0f, moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0f, 0f, moveSpeed * -1f);
+		}
+		Camera.main.transform.position = transform.position + new Vector3 (0f, 5f, -6f);
+	}
+}
+
+
+
+
